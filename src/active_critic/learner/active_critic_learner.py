95b8601d09364e5a0cfdbba3d986e0ef361b06dc
Hendrik Elvers <h.elvers@synergeticon.com>
2022-10-14 18:06:25 +0200

    I should not diable TF GPU globally on the server.

--- src/active_critic/learner/active_critic_learner.py
+++ src/active_critic/learner/active_critic_learner.py
@@ -55,17 +55,6 @@ class ActiveCriticLearner(nn.Module):
         self.scores = ACLScores()
 
         if network_args_obj.tboard:
-            gpus = tf.config.list_physical_devices('GPU')
-            if gpus:
-                try:
-                    # Currently, memory growth needs to be the same across GPUs
-                    for gpu in gpus:
-                        tf.config.experimental.set_memory_growth(gpu, True)
-                        logical_gpus = tf.config.list_logical_devices('GPU')
-                        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-                except RuntimeError as e:
-                    # Memory growth must be set before GPUs have been initialized
-                    print(e)
             self.tboard = TBoardGraphs(
                 self.logname, data_path=network_args_obj.data_path)
         self.global_step = 0
