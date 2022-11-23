0d30fb12d6fa510469bbcf0ccf1cc894c9789053
Hendrik Elvers <h.elvers@synergeticon.com>
2022-11-23 10:12:30 +0100

    asd

--- tests/learner/vis_hopper.py
+++ tests/learner/vis_hopper.py
@@ -3,7 +3,7 @@ from gym import envs
 import numpy as np
 
 if __name__ == '__main__':
-    env = gym.make('Hopper-v2')
+    env = gym.make('Hopper-v3')
     result = env.reset()
     for i in range(300):
         obs, rew, done, info = env.step(np.array([1,1,0]))
