ef23509f41e94bfd691b2ceae443438643976131
Hendrik Elvers <hendrik11ers@msn.com>
2022-12-09 02:44:02 +0100

    discrete actions, obsvs, rewards 512 reach

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -27,8 +27,8 @@ def make_wsm_setup(seq_len, d_output, device='cuda'):
     d_output = d_output
     wsm.model_setup.d_output = d_output
     wsm.model_setup.nhead = 4
-    wsm.model_setup.d_hid = 512
-    wsm.model_setup.d_model = 512
+    wsm.model_setup.d_hid = 64
+    wsm.model_setup.d_model = 64
     wsm.model_setup.nlayers = 6
     wsm.model_setup.seq_len = seq_len
     wsm.model_setup.dropout = 0
@@ -81,8 +81,8 @@ def setup_ac_reach(seq_len, num_cpu, env_tag, device):
 def make_acl(device, env_tag, logname):
     device = device
     acla = ActiveCriticLearnerArgs()
-    acla.data_path = '/data/bing/hendrik/'
-    #acla.data_path = '/home/hendrik/Documents/master_project/LokalData/'
+    #acla.data_path = '/data/bing/hendrik/'
+    acla.data_path = '/home/hendrik/Documents/master_project/LokalData/'
 
     acla.device = device
     acla.extractor = DummyExtractor()
@@ -92,7 +92,7 @@ def make_acl(device, env_tag, logname):
     acla.batch_size = 32
     acla.val_every = 1
     acla.add_data_every = 1
-    acla.validation_episodes = 20
+    acla.validation_episodes = 1
     acla.training_epsiodes = 1
     acla.actor_threshold = 5e-1
     acla.critic_threshold = 5e-1
