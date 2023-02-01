7906c708d48b6d46b2d0a144ca1de0dea25e22dc
Hendrik Elvers <h.elvers@synergeticon.com>
2023-02-01 02:41:19 +0100

    10 imitation long opt

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -48,7 +48,7 @@ def make_acps(seq_len, extractor, new_epoch, device, batch_size=32):
     acps.epoch_len = seq_len
     acps.extractor = extractor
     acps.new_epoch = new_epoch
-    acps.opt_steps = 200
+    acps.opt_steps = 10000
     acps.optimisation_threshold = 1
     acps.inference_opt_lr = 5e-3
     
@@ -84,7 +84,7 @@ def make_acl(device):
     acla.extractor = ReductiveExtractor()
     acla.imitation_phase = False
     tag = 'pickplace'
-    acla.logname = tag + ' 10 imitation'
+    acla.logname = tag + ' 10 imitation long opt'
     acla.tboard = True
     acla.batch_size = 16
     number = 10
@@ -94,7 +94,7 @@ def make_acl(device):
     acla.training_epsiodes = 10
     acla.actor_threshold = 1e-2
     acla.critic_threshold = 1e-3
-    acla.num_cpu = 25
+    acla.num_cpu = 100
 
     seq_len = 200
     epsiodes = 30
