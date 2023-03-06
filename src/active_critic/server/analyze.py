a5b815adc0a37606cd438c81510a0f44134b1f41
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-06 21:12:29 +0100

    8,12, 16 AC pick place

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -264,7 +264,7 @@ def run_eval_stats_demos(device, weight_decay):
 
 def run_eval_stats_pp(device, weight_decay):
     imitation_phases = [True]
-    demonstrations_list = [4, 8, 12]
+    demonstrations_list = [8, 12, 16]
     run_ids = [i for i in range(5)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
@@ -273,25 +273,24 @@ def run_eval_stats_pp(device, weight_decay):
     data_path = '/data/bing/hendrik/AC_var_' + s
     env_tags = ['pickplace']
     val_everys = [10000]
-    for i in range(len(env_tags)):
-        demonstrations = demonstrations_list[i]
-        env_tag = env_tags[i]
-        for im_ph in imitation_phases:
-            for val_every in val_everys:
-                for run_id in run_ids:
-                    logname = f' demonstrations: {demonstrations}, im_ph:{im_ph}, training_episodes: {training_episodes}, min critic: {min_critic_threshold}, wd: {weight_decay}, val_every: {val_every} run id: {run_id}'
-                    run_experiment(device=device,
-                                env_tag=env_tag,
-                                logname=logname,
-                                data_path=data_path,
-                                demos=demonstrations,
-                                imitation_phase=im_ph,
-                                total_training_epsiodes=total_training_epsiodes,
-                                training_episodes=training_episodes,
-                                min_critic_threshold=min_critic_threshold,
-                                weight_decay = weight_decay,
-                                val_every=val_every,
-                                fast=False)
+    for demonstrations in demonstrations_list:
+        for env_tag in env_tags:
+            for im_ph in imitation_phases:
+                for val_every in val_everys:
+                    for run_id in run_ids:
+                        logname = f' demonstrations: {demonstrations}, im_ph:{im_ph}, training_episodes: {training_episodes}, min critic: {min_critic_threshold}, wd: {weight_decay}, val_every: {val_every} run id: {run_id}'
+                        run_experiment(device=device,
+                                    env_tag=env_tag,
+                                    logname=logname,
+                                    data_path=data_path,
+                                    demos=demonstrations,
+                                    imitation_phase=im_ph,
+                                    total_training_epsiodes=total_training_epsiodes,
+                                    training_episodes=training_episodes,
+                                    min_critic_threshold=min_critic_threshold,
+                                    weight_decay = weight_decay,
+                                    val_every=val_every,
+                                    fast=False)
 
 if __name__ == '__main__':
     run_eval(device='cuda')
