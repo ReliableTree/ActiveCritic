d67eb07bd77179e83ea40214ce19463ffca33a60
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-21 13:38:49 +0100

    push 10 plot

--- src/active_critic/analyze_stats/analyze_stats.py
+++ src/active_critic/analyze_stats/analyze_stats.py
@@ -18,35 +18,6 @@ def parse_data(paths):
                 result_dict[key] = next_entrance
     return result_dict
 
-def plot_experiment_data(timesteps, experiments, names, plot_name, path=None):
-    # create figure and axis objects
-    fig, ax = plt.subplots()
-
-    # loop over experiments
-    for i, experiment in enumerate(experiments):
-        # calculate mean and standard deviation of each time step for this experiment
-        mean_data = np.mean(experiment, axis=0)
-        std_data = 1 / np.sqrt(experiment.shape[0]) * np.std(experiment, axis=0)
-
-        # plot mean data as a line and shade area between ±1 standard deviation
-        ax.plot(timesteps[i], mean_data, '-o', markersize=5, label=names[i])
-        ax.fill_between(timesteps[i], mean_data-std_data, mean_data+std_data, alpha=0.3)
-
-    # add labels, title, and legend to the plot
-    ax.set_xlabel('Number Sampled Trajectories')
-    ax.set_ylabel('Success Rate')
-    ax.set_title(plot_name)
-    ax.legend(loc = 'upper left')
-
-    if path is not None:
-        # create directory if it doesn't exist
-        os.makedirs(path, exist_ok=True)
-        # save the plot
-        plt.savefig(os.path.join(path, plot_name + '.png'))
-
-    # display the plot
-    plt.show()
-
 def file_crawler(path, substrings, exclude=[]):
     result = []
     for root, dirs, files in os.walk(path):
@@ -57,7 +28,55 @@ def file_crawler(path, substrings, exclude=[]):
     print(f'for path: {path}: {len(result)}')
     return result
 
-def make_plot(paths, includes, excludes, names, plot_name, save_path = None):
+def plot_experiment_data(timesteps, experiments, names, plot_name, path=None, plot_closest=False):
+    # create figure and axis objects
+    fig, ax = plt.subplots()
+
+    # find the experiment with the smallest number of timesteps n_timesteps with timestep values p_timesteps
+    min_timesteps = min([len(t) for t in timesteps])
+    p_timesteps = timesteps[[len(t) for t in timesteps].index(min_timesteps)]
+    # for all other experiments, find the n_timesteps timesteps, that are the closest to p_timesteps
+
+    new_experiments = []
+    if plot_closest:
+        for exp in range(len(experiments)):
+            if timesteps[exp][0] != 0:
+                experiments[exp] = np.concatenate((np.zeros_like(experiments[exp][:,:1]), experiments[exp]), axis=1)
+                timesteps[exp] = np.concatenate((np.zeros_like(timesteps[exp][:1]), timesteps[exp]), axis=0)
+            dist_timesteps = (timesteps[exp][None,:] - p_timesteps[:, None])**2
+            ind_new_timesteps = np.argmin(dist_timesteps, axis=1)
+            new_experiments.append(experiments[exp][:, ind_new_timesteps])
+    else:
+        new_experiments = experiments
+            
+
+    # loop over experiments
+    for i, experiment in enumerate(new_experiments):
+        # calculate mean and standard deviation of each time step for this experiment
+        mean_data = np.mean(experiment, axis=0)
+        std_data = 1 / np.sqrt(experiment.shape[0]) * np.std(experiment, axis=0)
+
+        # plot mean data as a line and shade area between ±1 standard deviation
+        if plot_closest:
+            # plot the experiments at those timesteps
+            ax.plot(p_timesteps, mean_data, '-o', markersize=5, label=names[i])
+            ax.fill_between(p_timesteps, mean_data-std_data, mean_data+std_data, alpha=0.3)
+        else:
+            ax.plot(timesteps[i], mean_data, '-o', markersize=5, label=names[i])
+            ax.fill_between(timesteps[i], mean_data-std_data, mean_data+std_data, alpha=0.3)
+
+    # add labels, title, and legend to the plot
+    ax.set_xlabel('Number Sampled Trajectories')
+    ax.set_ylabel('Success Rate')
+    ax.set_title(plot_name)
+    ax.legend()
+    if path is not None:
+        # create directory if it doesn't exist
+        os.makedirs(path, exist_ok=True)
+        # save the plot
+        plt.savefig(os.path.join(path, plot_name + '.png'))
+
+def make_plot(paths, includes, excludes, names, plot_name, save_path = None, plot_closest=False):
     abs_file_path_list = []
     
     for i in range(len(paths)):
@@ -72,5 +91,6 @@ def make_plot(paths, includes, excludes, names, plot_name, save_path = None):
         experiments=[result_dict['success_rate'] for result_dict in dicts],
         names=names,
         plot_name=plot_name,
-        path=save_path
+        path=save_path,
+        plot_closest=plot_closest
         )
---
+++ src/active_critic/analyze_stats/push_10_RPPO_TQC_PPO.py
@@ -0,0 +1,32 @@
+from active_critic.analyze_stats.analyze_stats import make_plot
+
+def make_final_plot():
+    AC_path = '/home/hendrik/Documents/master_project/LokalData/server/AC/AC_var_2023-02-23'
+    bl_path = '/home/hendrik/Documents/master_project/LokalData/server/baselines/Baselines_Stats_GAIL_2023-02-22/'
+    bl_RPPO_batch = '/home/hendrik/Documents/master_project/LokalData/server/baselines/Baselines_Stats_GAIL_2023-03-21'
+
+    include_ac = ['push demonstrations: 10, im_ph:False, training_episodes: 10, min critic: 5e-05, wd: 0.01, val_every: 5000', 'stats']
+    exclude_ac = ['optimized']
+
+
+
+    include_bl_PPO = ['demonstrations_10', 'learner', 'push', 'PPO', '0.0001']
+    exclude_bl = []
+
+    include_bl_TQC = ['TQC_GAIL_push_lr_1e-07_demonstrations_10', 'learner']
+    exclude_bl = []
+
+    include_bl_RPPO = ['RPPO_push_lr_1e-06_demonstrations_10_id', 'learner']
+    exclude_bl = []
+
+    save_path = '/home/hendrik/Documents/master_project/LokalData/server/Stats for MA/Push_TQC_PPO_RPPO_10/'
+
+    make_plot(
+        paths=[AC_path, bl_RPPO_batch, bl_path, bl_path], 
+        includes=[include_ac, include_bl_RPPO, include_bl_PPO, include_bl_TQC], 
+        excludes=[exclude_ac, exclude_bl, exclude_bl, exclude_bl],
+        names=['AC', 'RPPO', 'PPO + GAIL', 'TQC + GAIL'],
+        plot_name='Push Environment',
+        save_path = save_path,
+        plot_closest=True
+        )
