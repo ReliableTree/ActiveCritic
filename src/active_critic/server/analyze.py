4ba545189d4355cafceb0930aca8d4e3c63208f7
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-22 12:17:27 +0100

    asd

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -302,14 +302,14 @@ def run_eval_stats_pp(device, weight_decay):
 
 def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False, True]
-    demonstrations_list = [50]
+    demonstrations_list = [1]
     run_ids = [i for i in range(5)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
     total_training_epsiodes = 200
     min_critic_threshold = 5e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['pickplace']
+    env_tags = ['reach']
     val_everys = [10000]
     opt_modes = ['actor', 'actions']
     for demonstrations in demonstrations_list:
