4a495d234f450b498191c19f87361cbac6c8f0ec
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-23 18:43:10 +0100

    reach, window open 1, 1000

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -372,10 +372,10 @@ def run_eval_stats_env(device, weight_decay):
     run_ids = [i for i in range(1)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 100000
+    total_training_epsiodes = 1000
     min_critic_threshold = 5e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['pickplace']
+    env_tags = ['reach', 'windowopen']
     val_everys = [6000]
     add_data_everys = [6000]
     opt_modes = ['actor+plan']
