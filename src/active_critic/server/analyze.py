27d1956ef7cd2d96e9b28f2e252de08726216833
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-24 15:51:09 +0100

    ['reach', 'windowopen', 'push', 'pickplace']

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -368,14 +368,14 @@ def run_eval_stats_pp(device, weight_decay):
 
 def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False]
-    demonstrations_list = [1]
-    run_ids = [i for i in range(1)]
+    demonstrations_list = [4]
+    run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 2000
+    total_training_epsiodes = 400
     min_critic_threshold = 5e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['pickplace']
+    env_tags = ['reach', 'windowopen', 'push', 'pickplace']
     val_everys = [6000]
     add_data_everys = [6000]
     opt_modes = ['actor+plan']
@@ -386,7 +386,7 @@ def run_eval_stats_env(device, weight_decay):
                     for run_id in run_ids:
                         for opt_mode in opt_modes:
                             logname = f'tiny planner trainin eps: {total_training_epsiodes} opt mode: {opt_mode} demonstrations: {demonstrations}, im_ph:{im_ph}, training_episodes: {training_episodes}, min critic: {min_critic_threshold}, wd: {weight_decay}, val_every: {val_every} run id: {run_id}'
-                            print(f'____________________________________logname: env_tag {logname}')
+                            print(f'____________________________________logname: {logname}')
                             run_experiment(device=device,
                                         env_tag=env_tag,
                                         logname=logname,
