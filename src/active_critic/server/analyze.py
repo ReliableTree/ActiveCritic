967a9ca9b91913625666b8873cad9575b8b60d5f
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-24 22:33:57 +0100

    ['pickplace', 'reach', 'push'] 15 400

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -369,14 +369,14 @@ def run_eval_stats_pp(device, weight_decay):
 
 def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False]
-    demonstrations_list = [1]
-    run_ids = [i for i in range(2)]
+    demonstrations_list = [15]
+    run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 1000
+    total_training_epsiodes = 400
     min_critic_threshold = 5e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['windowopen', 'reach']
+    env_tags = ['pickplace', 'reach', 'push']
     val_everys = [6000]
     add_data_everys = [6000]
     opt_modes = ['actor+plan']
