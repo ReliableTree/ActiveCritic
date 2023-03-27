fdcf3c84b24c6d785bb7f9bfa72f45579bba540e
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-28 00:27:12 +0200

    drawerclose 1000 0

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -373,7 +373,7 @@ def run_eval_stats_pp(device, weight_decay):
 def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False]
     demonstrations_list = [0]
-    th.manual_seed(1)
+    th.manual_seed(0)
     run_ids = [i for i in range(4)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
@@ -390,7 +390,7 @@ def run_eval_stats_env(device, weight_decay):
                 for im_ph in imitation_phases:
                     for val_step, val_every in enumerate(val_everys):
                         for opt_mode in opt_modes:
-                            logname = f' sm manual_seed(1) no train inf eps: {total_training_epsiodes} opt mode: {opt_mode} demonstrations: {demonstrations}, im_ph:{im_ph}, training_episodes: {training_episodes}, min critic: {min_critic_threshold}, wd: {weight_decay}, val_every: {val_every} run id: {run_id}'
+                            logname = f' sm manual_seed(0) no train inf eps: {total_training_epsiodes} opt mode: {opt_mode} demonstrations: {demonstrations}, im_ph:{im_ph}, training_episodes: {training_episodes}, min critic: {min_critic_threshold}, wd: {weight_decay}, val_every: {val_every} run id: {run_id}'
                             print(f'____________________________________logname: {env_tag}  {logname}')
                             run_experiment(device=device,
                                         env_tag=env_tag,
