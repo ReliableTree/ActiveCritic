be3503aa70504d335f61aa07ad8505c97bd77430
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-28 18:53:28 +0200

    AC pickplace 15 5000

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -374,13 +374,13 @@ def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False]
     demonstrations_list = [15]
     th.manual_seed(1)
-    run_ids = [i for i in range(4)]
+    run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 400
+    total_training_epsiodes = 5000
     min_critic_threshold = 5e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['windowopen']
+    env_tags = ['pickplace']
     val_everys = [6000]
     add_data_everys = [6000]
     opt_modes = ['actor+plan']
