0bda75e4d3b9204098c5695b3ec0d55911c611be
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-29 23:28:22 +0200

    push pickplace sparse AC

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -391,14 +391,14 @@ def run_eval_stats_pp(device, weight_decay):
 
 def run_eval_stats_env(device, weight_decay):
     imitation_phases = [False]
-    demonstrations_list = [1]
+    demonstrations_list = [3]
     run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 1000
+    total_training_epsiodes = 2000
     min_critic_threshold = 1e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['reach', 'windowopen']
+    env_tags = ['pickplace', 'push']
     val_everys = [1000]
     add_data_everys = [1000]
     opt_modes = ['actor+plan']
