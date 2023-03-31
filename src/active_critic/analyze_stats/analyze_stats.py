b3fee4293136d847fe19915ca81953ec66508a25
Hendrik Elvers <h.elvers@synergeticon.com>
2023-03-31 13:36:41 +0200

    asd

---
+++ src/active_critic/analyze_stats/analyze_stats.py
@@ -0,0 +1,96 @@
+import pickle
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+def parse_data(paths):
+    result_dict = {}
+    dicts = []
+    for path in paths:
+        with open(path, 'rb') as f:
+            dicts.append(pickle.load(f))
+    for dict in dicts:
+        for key in dict:
+            if key != 'rewards':
+                next_entrance = dict[key].reshape([1, -1])
+            if key in result_dict:
+                result_dict[key] = np.append(result_dict[key], next_entrance, axis=0)
+            else:
+                result_dict[key] = next_entrance
+    return result_dict
+
+def file_crawler(path, substrings, exclude=[]):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            file_path = os.path.join(root, name)
+            if all(s in file_path for s in substrings) and not any(e in file_path for e in exclude):
+                result.append(file_path)
+    print(f'for path: {path}: {len(result)}')
+    return result
+
+def plot_experiment_data(timesteps, experiments, names, plot_name, path=None, plot_closest=False):
+    # create figure and axis objects
+    fig, ax = plt.subplots()
+
+    # find the experiment with the smallest number of timesteps n_timesteps with timestep values p_timesteps
+    min_timesteps = min([len(t) for t in timesteps])
+    p_timesteps = timesteps[[len(t) for t in timesteps].index(min_timesteps)]
+    # for all other experiments, find the n_timesteps timesteps, that are the closest to p_timesteps
+
+    new_experiments = []
+    if plot_closest:
+        for exp in range(len(experiments)):
+            if timesteps[exp][0] != 0:
+                experiments[exp] = np.concatenate((np.zeros_like(experiments[exp][:,:1]), experiments[exp]), axis=1)
+                timesteps[exp] = np.concatenate((np.zeros_like(timesteps[exp][:1]), timesteps[exp]), axis=0)
+            dist_timesteps = (timesteps[exp][None,:] - p_timesteps[:, None])**2
+            ind_new_timesteps = np.argmin(dist_timesteps, axis=1)
+            new_experiments.append(experiments[exp][:, ind_new_timesteps])
+    else:
+        new_experiments = experiments
+            
+
+    # loop over experiments
+    for i, experiment in enumerate(new_experiments):
+        # calculate mean and standard deviation of each time step for this experiment
+        mean_data = np.mean(experiment, axis=0)
+        std_data = 1 / np.sqrt(experiment.shape[0]) * np.std(experiment, axis=0)
+
+        # plot mean data as a line and shade area between ±1 standard deviation
+        if plot_closest:
+            # plot the experiments at those timesteps
+            ax.plot(p_timesteps, mean_data, '-o', markersize=5, label=names[i])
+            ax.fill_between(p_timesteps, mean_data-std_data, mean_data+std_data, alpha=0.3)
+        else:
+            ax.plot(timesteps[i], mean_data, '-o', markersize=5, label=names[i])
+            ax.fill_between(timesteps[i], mean_data-std_data, mean_data+std_data, alpha=0.3)
+
+    # add labels, title, and legend to the plot
+    ax.set_xlabel('Number Sampled Trajectories')
+    ax.set_ylabel('Success Rate')
+    ax.set_title(plot_name)
+    ax.legend()
+    if path is not None:
+        # create directory if it doesn't exist
+        os.makedirs(path, exist_ok=True)
+        # save the plot
+        plt.savefig(os.path.join(path, plot_name + '.png'))
+
+def make_plot(paths, includes, excludes, names, plot_name, save_path = None, plot_closest=False):
+    abs_file_path_list = []
+    
+    for i in range(len(paths)):
+        abs_file_path_list.append(file_crawler(path=paths[i], substrings=includes[i], exclude=excludes[i]))
+    dicts = []
+    
+    for result in abs_file_path_list:
+        dicts.append(parse_data(paths=result))
+    plot_experiment_data(
+        timesteps=[result_dict['step'][0] for result_dict in dicts], 
+        experiments=[result_dict['success_rate'] for result_dict in dicts],
+        names=names,
+        plot_name=plot_name,
+        path=save_path,
+        plot_closest=plot_closest
+        )
