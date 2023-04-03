962ec0c25ae39ae88626d72b826377f35e0d5921
Hendrik Elvers <h.elvers@synergeticon.com>
2023-04-03 12:21:39 +0200

    AC windowopen no exp

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -283,10 +283,10 @@ def run_eval_stats_env(device, weight_decay):
     run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 1000
+    total_training_epsiodes = 5000
     min_critic_threshold = 1e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['drawerclose']
+    env_tags = ['windowopen']
     val_everys = [1000]
     add_data_everys = [1000]
     opt_modes = ['actor+plan']
@@ -295,7 +295,7 @@ def run_eval_stats_env(device, weight_decay):
     seq_len = 100
     max_epoch_steps = 15000
     manual_seed = 1
-    explore_until = 30
+    explore_until = 100
     th.manual_seed(manual_seed)
     for demonstrations in demonstrations_list:
         for env_tag in env_tags:
