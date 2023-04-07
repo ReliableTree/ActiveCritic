5333ab4eab9a30b190ca034f096ad6163d907da4
Hendrik Elvers <h.elvers@synergeticon.com>
2023-04-07 11:38:27 +0200

    before hack

--- src/active_critic/learner/active_critic_learner.py
+++ src/active_critic/learner/active_critic_learner.py
@@ -96,7 +96,7 @@ class ActiveCriticLearner(nn.Module):
                 self.logname + ' optimized', data_path=network_args_obj.data_path)
         self.global_step = 0
 
-        self.train_data = DatasetAC(batch_size=self.network_args.batch_size, device='cpu', max_size=self.network_args.max_epoch_steps)
+        self.train_data = DatasetAC(batch_size=self.network_args.batch_size, device='cpu', max_size=None)
         self.train_data.onyl_positiv = False
         self.exp_dict_opt = None
         self.exp_dict = None
