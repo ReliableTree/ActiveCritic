80f9a5a674e41d8e4338c35433b4f6a3dd6816b8
Hendrik Elvers <h.elvers@synergeticon.com>
2023-04-27 15:38:57 +0200

    PPO

--- baselines/hendrik_eval.py
+++ baselines/hendrik_eval.py
@@ -648,14 +648,14 @@ if __name__ == '__main__':
     s = datetime.today().strftime('%Y-%m-%d')
 
     list_demonstrations = [0]
-    list_env_tags = ['windowopen']
-    n_samples = 2000
+    list_env_tags = ['reach']
+    n_samples = 5000
     bc_epochs = 0
     ids = [i for i in range(3)]
     dense_list = [True]
     dense = True
     sparse = True
-    ms = 1
+    ms = 0
     th.manual_seed(ms)
     np.random.seed(ms)
     seq_len = 100
