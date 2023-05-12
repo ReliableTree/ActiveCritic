23a11a2fc1464c882d36c08e127ea546fc041b5c
Hendrik Elvers <h.elvers@synergeticon.com>
2023-05-12 11:25:00 +0200

    reach 5000 1

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -198,9 +198,9 @@ def make_acl(
 
     acla.use_pred_loss = True
     acla.explore_cautious_until = 3
-    acla.exploid_inf_lr = 1e-7
-    acla.explore_inf_lr = 1e-7
-    acla.update_inf_lr_every = 100
+    acla.exploid_inf_lr = 1e-6
+    acla.explore_inf_lr = 1e-6
+    acla.update_inf_lr_every = 1000000
     acla.update_int_lr_factor = 2
 
 
@@ -288,21 +288,21 @@ def run_experiment(
 def run_eval_stats_env(device, ms):
     weight_decay = 1e-2
     imitation_phases = [False]
-    demonstrations_list = [4]
+    demonstrations_list = [1]
     run_ids = [i for i in range(1)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 100000
+    total_training_epsiodes = 5000
     min_critic_threshold = 1e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
-    env_tags = ['pickplace']
+    env_tags = ['reach']
     val_everys = [1000]
     add_data_everys = [1000]
     opt_modes = ['actor+plan']
-    opt_steps_list = [1]
+    opt_steps_list = [3]
     sparse = True
     seq_len = 100
-    max_epoch_steps = 30000
+    max_epoch_steps = 15000
     manual_seed = ms
     explore_until = 0
     th.manual_seed(manual_seed)
