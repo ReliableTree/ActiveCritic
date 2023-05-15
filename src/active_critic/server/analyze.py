58ba1452c0dd78ae3d9ef433dae8f5963b486779
Hendrik Elvers <h.elvers@synergeticon.com>
2023-05-15 11:23:47 +0200

    reach 0 5000

--- src/active_critic/server/analyze.py
+++ src/active_critic/server/analyze.py
@@ -285,7 +285,7 @@ def run_eval_stats_env(device, ms):
     run_ids = [i for i in range(3)]
     s = datetime.today().strftime('%Y-%m-%d')
     training_episodes = 10
-    total_training_epsiodes = 2000
+    total_training_epsiodes = 5000
     min_critic_threshold = 1e-5
     data_path = '/data/bing/hendrik/AC_var_' + s
     env_tags = ['reach']
